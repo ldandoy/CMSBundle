2326f523e4c8d7f228fe13005d036ea05eb11e03
ldandoy <ldandoy@overconsulting.net>
2012-11-19 14:06:57 +0100

    ajout partie CMS

---
+++ Controller/AdminCMSController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace StartPack\CMSBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use StartPack\CoreBundle\Entity as Model;
+use StartPack\CoreBundle\Form as Form;
+
+use StartPack\CoreBundle\Controller\AbstractCoreController;
+
+ /**
+     * @Route("/admin/cms")
+     */
+class AdminCMSController extends AbstractCoreController
+{
+    /**
+     * @Route("/", name="admin_cms_index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+    	$pages = $this->getDoctrine()->getRepository('CoreBundle:Page')->findAll();
+        
+        return array(
+        	'pages'     =>	$pages,
+        	'activeCMS'	=>	true
+        );
+    }
+
+    /**
+     * @Route("/{id}/view", name="admin_cms_view")
+     * @Template()
+     */
+    public function viewAction(Model\Page $page)
+    {
+        return array(
+            "page"  =>  $page,
+            'activeCMS' => true
+        );
+    }
+}
---
+++ Controller/CMSController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace StartPack\CMSBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use StartPack\CoreBundle\Entity as Model;
+use StartPack\CoreBundle\Form as Form;
+
+use StartPack\CoreBundle\Controller\AbstractCoreController;
+
+ /**
+     * @Route("/page/{slug}")
+     */
+class CMSController extends AbstractCoreController
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction($slug)
+    {
+    	$page = $this->getDoctrine()->getRepository('CoreBundle:Page')->findOneBySlug($slug);
+        
+        return array(
+        	'page'     => $page,
+        	'slug'     => $slug
+        );
+    }
+}
